b650a5f1ef2c022dfa9852e1d09129fc9b29927a
rockan007 <rockan007@gmail.com>
2020-03-21 20:30:40 +0800

    文件读取 websocket依赖

---
+++ extract.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var extractFilePath = function (url) {
+    var filePath;
+    var fileName = 'index.html';
+
+    if (url.length > 1) {
+        fileName = url.substring(1);
+    }
+    console.log("the File name is:" + fileName);
+
+    filePath = path.resolve(__dirname, 'app', fileName);
+    return filePath;
+};
+module.exports = extractFilePath;
---
+++ index.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var fs = require('fs');
+var extract = require('./extract');
+var handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+}
+var server = http.createServer(function (req, res) {
+    console.log('Responding to a request:');
+    var url = req.url;
+    var filePath = extract(url);
+    fs.readFile(filePath, function (err, data) {
+        if (err) {
+            handleError(err, res);
+            return;
+        }
+        res.setHeader("Content-Type",'text/html');
+        res.end(data);
+    })
+})
+server.listen(3000);
