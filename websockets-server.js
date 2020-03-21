b05365cb63064b7cea8f7a2b1ee20047f2a274b7
rockan007 <rockan007@gmail.com>
2020-03-21 21:15:27 +0800

    websocket练习

--- websockets-server.js
+++ websockets-server.js
@@ -1,12 +1,43 @@
-var WebSocket =require('ws');
-var WebSocketServer =WebSocket.Server;
-var port=3001;
+var WebSocket = require('ws');
+var WebSocketServer = WebSocket.Server;
+var port = 3001;
 var ws = new WebSocketServer({
-    port:port
+    port: port
 });
+var messages = [];
 console.log(
     "websockets server started"
 );
-ws.on("connection",function(socket){
+
+ws.on("connection", function (socket) {
     console.log("client connection established");
+
+    socket.on('message', function (data) {
+        console.log("message received:" + data);
+
+        if (data == "Swordfish") {
+            socket.send("欢迎加入组织！");
+            //密码正确后，将所有消息发送给新的连接
+            messages.forEach(function (msg) {
+                socket.send(msg);
+            })
+            socket.isSecret = true;
+           
+            return;
+        }
+        if (socket.isSecret) {
+            messages.push(data);
+        } else {
+            socket.send("请输入暗号,加入组织！");
+        }
+
+
+        //ws的clients属性记录了所有连接，当接收到新消息时，将新消息发送给所有用户
+        ws.clients.forEach(function (clientSocket) {
+            if (clientSocket.isSecret) {
+                clientSocket.send(data);
+            }
+
+        })
+    })
 })
